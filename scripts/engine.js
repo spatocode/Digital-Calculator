90ca0a545091fb54428f5f504bd995eb0915ac45
Spatocode <impulseofficial069@gmail.com>
2018-08-12 20:33:30 +0100

    Single input calculation logics implemented

--- scripts/engine.js
+++ scripts/engine.js
@@ -43,5 +43,149 @@ var engine =  {
     },
     operate: () => {
         
-    }
+    },
+    addition: () => {
+        joinItems= itemDisplay.join("")
+        while(itemDisplay.length > 0) {
+            itemDisplay.pop()
+            $('#currentValue').html(itemDisplay)
+        }
+        $('#calc').html(`${joinItems} +`)
+    },
+    division: () => {
+        joinItems= itemDisplay.join("")
+        while(itemDisplay.length > 0) {
+            itemDisplay.pop()
+            $('#currentValue').html(itemDisplay)
+        }
+        $('#calc').html(`${joinItems} /`)
+    },
+    multiply: () => {
+        joinItems= itemDisplay.join("")
+        while(itemDisplay.length > 0) {
+            itemDisplay.pop()
+            $('#currentValue').html(itemDisplay)
+        }
+        $('#calc').html(`${joinItems} *`)
+    },
+    subtract: () => {
+        joinItems= itemDisplay.join("")
+        while(itemDisplay.length > 0) {
+            itemDisplay.pop()
+            $('#currentValue').html(itemDisplay)
+        }
+        $('#calc').html(`${joinItems} -`)
+    },
+    cube: () => {
+        joinItems = itemDisplay.join("")
+        let cube = Math.pow(joinItems, 3)
+
+        while(itemDisplay.length > 0) {
+            itemDisplay.pop()
+            $('#currentValue').html(itemDisplay)
+        }
+
+        itemDisplay.push(cube)
+        err.textContent = ""
+        
+        if (cube.toString().length >= 14)
+        $('#currentValue').css({"font-size":"36px","position":"absolute","top":"13px"})
+
+        if (cube.toString().length > 16)
+        $('#currentValue').css({"font-size":"34px","position":"absolute","top":"18px"})
+
+        if (cube.toString().length >= 18)
+        $('#currentValue').css({"font-size":"28px","position":"absolute","top":"18px"})
+
+        if (cube.toString().length >= 21)
+        $('#currentValue').css({"font-size":"26px","position":"absolute","top":"18px"})
+
+        console.log(cube.toString().length)
+
+        $('#calc').html(`Cube(${joinItems})`)
+        $('#currentValue').html(itemDisplay)
+    },
+    negative: () => {
+        joinItems = itemDisplay.join("")
+        let neg = 1 / joinItems
+
+        while(itemDisplay.length > 0) {
+            itemDisplay.pop()
+            $('#currentValue').html(itemDisplay)
+        }
+
+        itemDisplay.push(neg)
+        err.textContent = ""
+
+        if (neg.toString().length >= 14)
+        $('#currentValue').css({"font-size":"36px","position":"absolute","top":"13px"})
+
+        if (neg.toString().length > 16)
+        $('#currentValue').css({"font-size":"34px","position":"absolute","top":"18px"})
+
+        if (neg.toString().length > 18)
+        $('#currentValue').css({"font-size":"28px","position":"absolute","top":"18px"})
+
+        if (neg.toString().length >= 21)
+        $('#currentValue').css({"font-size":"26px","position":"absolute","top":"18px"})
+
+        $('#calc').html(`1/(${joinItems})`)
+        $('#currentValue').html(itemDisplay)
+    },
+    square: () => {
+        joinItems = itemDisplay.join("")
+        var square = Math.pow(joinItems, 2)
+
+        while(itemDisplay.length > 0) {
+            itemDisplay.pop()
+            $('#currentValue').html(itemDisplay)
+        }
+
+        itemDisplay.push(square)
+        err.textContent = ""
+
+        if (square.toString().length >= 14)
+        $('#currentValue').css({"font-size":"36px","position":"absolute","top":"13px"})
+
+        if (square.toString().length > 16)
+        $('#currentValue').css({"font-size":"34px","position":"absolute","top":"18px"})
+
+        if (square.toString().length > 18)
+        $('#currentValue').css({"font-size":"28px","position":"absolute","top":"18px"})
+
+        if (square.toString().length >= 21)
+        $('#currentValue').css({"font-size":"26px","position":"absolute","top":"18px"})
+
+        $('#calc').html(`Square(${joinItems})`)
+        $('#currentValue').html(itemDisplay)
+    },
+    squareRoot: () => {
+        joinItems = itemDisplay.join("")
+        var squareRt = Math.sqrt(joinItems)
+
+        while(itemDisplay.length > 0) {
+            itemDisplay.pop()
+            $('#currentValue').html(itemDisplay)
+        }
+
+        itemDisplay.push(squareRt)
+        err.textContent = ""
+
+        if (squareRt.toString().length > 14)
+        $('#currentValue').css({"font-size":"36px","position":"absolute","top":"13px"})
+
+        if (squareRt.toString().length > 16)
+        $('#currentValue').css({"font-size":"34px","position":"absolute","top":"18px"})
+
+        if (squareRt.toString().length > 18)
+        $('#currentValue').css({"font-size":"28px","position":"absolute","top":"18px"})
+
+        if (squareRt.toString().length >= 21)
+        $('#currentValue').css({"font-size":"26px","position":"absolute","top":"18px"})
+
+        $('#calc').html(`SquareRt(${joinItems})`)
+
+        $('#currentValue').html(itemDisplay)
+    },
+    
 }
